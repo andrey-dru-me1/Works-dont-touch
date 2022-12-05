f4935adb334b47fb35e89b25fee9d49312ab5099
Dmitrii Ponomarev <sidey383@mail.ru>
2022-12-05 23:31:07 +0700

    clear branch

--- JavaServer/src/main/java/ru/works/dont/touch/server/data/repositories/ImageRepository.java
+++
@@ -1,8 +0,0 @@
-package ru.works.dont.touch.server.data.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import ru.works.dont.touch.server.data.entities.Image;
-
-public interface ImageRepository extends CrudRepository<Image, Integer> {
-    Image findById(Long id);
-}
