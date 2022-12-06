cc2e463d7b74b5f30107eee7eca3e5c548c7cce9
andrey-dru-me1 <a.melnikov4@g.nsu.ru>
2022-12-07 01:53:02 +0700

    change image explorer picker to image photo picker

--- KotlinClient/app/src/main/java/ru/nsu/worksdonttouch/cardholder/kotlinclient/ui/AddCardActivity.kt
+++ KotlinClient/app/src/main/java/ru/nsu/worksdonttouch/cardholder/kotlinclient/ui/AddCardActivity.kt
@@ -1,5 +1,6 @@
 package ru.nsu.worksdonttouch.cardholder.kotlinclient.ui
 
+import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
@@ -8,20 +9,26 @@ import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import ru.nsu.worksdonttouch.cardholder.kotlinclient.data.DataController
-import ru.nsu.worksdonttouch.cardholder.kotlinclient.data.interaction.ImageSaver
 import ru.nsu.worksdonttouch.cardholder.kotlinclient.data.objects.Card
 import ru.nsu.worksdonttouch.cardholder.kotlinclient.ui.theme.KotlinClientTheme
 import java.io.File
+import java.io.FileOutputStream
+import java.io.OutputStream
 import java.nio.file.Files
 import java.nio.file.Paths
 
@@ -30,7 +37,6 @@ class AddCardActivity : ComponentActivity() {
 
     var cardName: String = ""
     var barCode: String = ""
-    var image: Uri? = null
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -43,10 +49,32 @@ class AddCardActivity : ComponentActivity() {
                     color = MaterialTheme.colors.background
                 ) {
                     Column {
+                        val bitmap: MutableState<Bitmap?> = remember { mutableStateOf(null) }
+                        val launcher = rememberLauncherForActivityResult(
+                            ActivityResultContracts.TakePicturePreview()
+                        )
+                        {
+                            bitmap.value = it
+                        }
+
                         CardNameEdit()
                         BarCodeEdit()
-                        PickImageButton()
-                        SaveButton()
+                        Button(
+                            onClick = { launcher.launch(null) },
+                            content = { Text("Pick an image") }
+                        )
+                        SaveButton(bitmap.value)
+                        bitmap.value?.asImageBitmap()?.let {
+                            Image(
+                                bitmap = it,
+                                contentDescription = "Picked image",
+                                contentScale = ContentScale.FillWidth,
+                                modifier = Modifier
+                                    .aspectRatio((86.0/54).toFloat())
+                                    .fillMaxWidth()
+                                    .clip(RoundedCornerShape(10.dp))
+                            )
+                        }
                     }
                 }
             }
@@ -84,31 +112,25 @@ class AddCardActivity : ComponentActivity() {
         )
     }
 
-    @Composable
-    fun PickImageButton() {
-        val launcher =
-            rememberLauncherForActivityResult(ActivityResultContracts.GetContent()) {
-                image = it
-            }
-        Button(onClick = {launcher.launch("image/*") }) {
-            Text("Pick an image")
-        }
-    }
-
     @RequiresApi(Build.VERSION_CODES.O)
     @Composable
-    fun SaveButton() {
+    fun SaveButton(bitmap: Bitmap?) {
         Button(onClick = {
-            val file = File("/data/data/ru.nsu.worksdonttouch.cardholder.kotlinclient/files/images/${cardName}")
+            val path: String = "/data/data/ru.nsu.worksdonttouch.cardholder.kotlinclient/files/images/${cardName}"
+            val file = File(path)
             try {
                 Files.createDirectory(Paths.get("/data/data/ru.nsu.worksdonttouch.cardholder.kotlinclient/files/images/"))
             }
             catch (_: Throwable) { }
             file.createNewFile()
 
-            if(image != null) ImageSaver.saveToFile(this, image!!, file)
+            val stream: OutputStream = FileOutputStream(file)
+            bitmap?.compress(Bitmap.CompressFormat.JPEG,100,stream)
+            stream.flush()
+            stream.close()
+            val uri: Uri = Uri.fromFile(file)
 
-            DataController.getInstance().putCard(Card(cardName, barCode, Uri.fromFile(file)))
+            DataController.getInstance().putCard(Card(cardName, barCode, uri))
             finish()
         }) {
             Text("OK")
