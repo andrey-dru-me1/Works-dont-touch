964c3dca8727449371b37c33de6a15357c841203
Dmitrii Ponomarev <sidey383@mail.ru>
2022-12-08 03:39:45 +0700

    some changes, go sleep zzz....

--- KotlinClient/app/src/main/java/ru/nsu/worksdonttouch/cardholder/kotlinclient/data/DataController.java
+++ KotlinClient/app/src/main/java/ru/nsu/worksdonttouch/cardholder/kotlinclient/data/DataController.java
@@ -1,9 +1,12 @@
 package ru.nsu.worksdonttouch.cardholder.kotlinclient.data;
 
 
+import java.io.File;
+import java.io.IOException;
 import java.util.logging.Logger;
 
 import ru.nsu.worksdonttouch.cardholder.kotlinclient.data.data.UserData;
+import ru.nsu.worksdonttouch.cardholder.kotlinclient.data.data.card.Card;
 import ru.nsu.worksdonttouch.cardholder.kotlinclient.net.ApiWorker;
 
 public class DataController {
@@ -14,18 +17,47 @@ public class DataController {
 
     private ApiWorker apiWorker = null;
 
-    public static synchronized void init() {
-        if(instance == null) {
+    private final DataFileContainer dataFileContainer;
 
-        }
+    private DataController(File dir) throws IOException {
+        dataFileContainer = new DataFileContainer(dir);
+    }
+
+    public static synchronized void init(File dir) throws IOException{
+        instance = new DataController(dir);
     }
 
     public static synchronized DataController getInstance() {
         return instance;
     }
 
-    private void setUser(UserData user) {
+    private void loginUser(UserData user) throws Exception {
+        apiWorker = ApiWorker.login(user);
+    }
 
+    private void registerUser(UserData user) throws Exception {
+        apiWorker = ApiWorker.register(user);
+    }
+
+    public void createCard(Card card, DataCallBack<Card> callBack) throws IllegalStateException, Exception {
+        if (apiWorker == null)
+            throw new IllegalStateException("Not authorized");
+        apiWorker.addCard(card, (result, data) -> {
+            switch (result) {
+                case FAIL:
+
+                case NO_PERMISSION:
+                    callBack.callback(DataCallBack.DataStatus.CANCELED, null);
+                    break;
+                case AUTHORIZATION_ERROR:
+                    callBack.callback(DataCallBack.DataStatus.WRONG_USER, null);
+                    case
+            }
+        });
+    }
+
+    public void pushUpdates() {
+        //TODO: реализовать
     }
 
 }
