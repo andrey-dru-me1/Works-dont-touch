689383532aa9657bc85fcba70837505e6f2e9ace
ttrubinov <t.trubinov@g.nsu.ru>
2022-12-08 14:33:55 +0700

    More requests

--- KotlinClient/app/src/main/java/ru/nsu/worksdonttouch/cardholder/kotlinclient/net/ApiWorker.java
+++ KotlinClient/app/src/main/java/ru/nsu/worksdonttouch/cardholder/kotlinclient/net/ApiWorker.java
@@ -1,11 +1,7 @@
 package ru.nsu.worksdonttouch.cardholder.kotlinclient.net;
 
 import android.os.Build;
-import com.squareup.moshi.JsonAdapter;
-import com.squareup.moshi.Moshi;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
+import okhttp3.*;
 import ru.nsu.worksdonttouch.cardholder.kotlinclient.data.data.UserData;
 import ru.nsu.worksdonttouch.cardholder.kotlinclient.data.data.card.Card;
 import ru.nsu.worksdonttouch.cardholder.kotlinclient.data.data.card.CardList;
@@ -19,41 +15,26 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
-import java.util.Map;
-import java.util.Objects;
 
 @SuppressWarnings("unused")
 public abstract class ApiWorker {
-
-    protected UserData user;
-    private static ApiWorker apiWorker;
-
-    static class Gist {
-        Map<String, GistFile> files;
-
-    }
-    static class GistFile {
-        String content;
-
-    }
     private static final OkHttpClient client = new OkHttpClient();
-    private final Moshi moshi = new Moshi.Builder().build();
-    private final JsonAdapter<Gist> gistJsonAdapter = moshi.adapter(Gist.class);
 
 
-    static String authorizationString(String login, String password) {
+    static String authorizationString(UserData data) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             return "Basic " + new String(Base64.getEncoder()
-                    .encode((login + ":" + password).getBytes(StandardCharsets.UTF_8)));
+                    .encode((data.getLogin() + ":" + data.getPassword()).getBytes(StandardCharsets.UTF_8)));
         }
         return null;
     }
 
-    public static ApiWorker login(UserData data) throws Exception {
+    public static ApiWorker authTest(UserData data) throws Exception {
+        RequestBody formBody = new FormBody.Builder().build();
         Request request = new Request.Builder()
                 .url("http://localhost:8080/v1.0/")
-                .addHeader("Authorization",
-                        Objects.requireNonNull(authorizationString(data.getLogin(), data.getPassword())))
+                .addHeader("Authorization", authorizationString(data))
+                .post(formBody)
                 .build();
 
         try (Response response = client.newCall(request).execute()) {
@@ -66,8 +47,9 @@ public abstract class ApiWorker {
             }
 
             if (response.body().string().equals("true")) {
-                return new ApiRequests();
-            } else {
+                return new ApiRequests(data);
+            }
+            else {
                 throw new NotAuthorizedException();
             }
 
@@ -77,9 +59,29 @@ public abstract class ApiWorker {
         }
     }
 
-    public static ApiWorker register(UserData data) throws Exception {
-        apiWorker.user = data;
-        return apiWorker;
+    public static ApiWorker registration(UserData data) throws Exception {
+        RequestBody formBody = new FormBody.Builder().build();
+        Request request = new Request.Builder()
+                .url("http://localhost:8080/v1.0/")
+                .addHeader("Login", data.getLogin())
+                .addHeader("Password", data.getPassword())
+                .post(formBody)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful()) {
+                throw new IOException("Unexpected code " + response);
+            }
+
+            if (response.body() == null) {
+                throw new IOException("No response");
+            }
+            return new ApiRequests(data);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new ServerConnectionException();
+        }
     }
 
     public abstract void changePassword(String password, HttpCallback<SimpleHttpResult> callback) throws Exception;
@@ -100,6 +102,7 @@ public abstract class ApiWorker {
      * **/
     public abstract void imageGet(long id, File toWrite, HttpCallback<File> callback) throws Exception;
 
-    public abstract void uploadImage(long id, HttpCallback<ImageAnswer> callback) throws Exception;
+    public abstract void uploadImage(File file, long id, HttpCallback<ImageAnswer> callback) throws Exception;
 
+    public abstract void editImage(File file, long id, HttpCallback<ImageAnswer> callback) throws Exception;
 }
--- KotlinClient/app/src/main/java/ru/nsu/worksdonttouch/cardholder/kotlinclient/net/requests/ApiRequests.java
+++ KotlinClient/app/src/main/java/ru/nsu/worksdonttouch/cardholder/kotlinclient/net/requests/ApiRequests.java
@@ -1,8 +1,10 @@
 package ru.nsu.worksdonttouch.cardholder.kotlinclient.net.requests;
 
+import android.os.Build;
 import com.squareup.moshi.JsonAdapter;
 import com.squareup.moshi.Moshi;
 import okhttp3.*;
+import ru.nsu.worksdonttouch.cardholder.kotlinclient.data.data.UserData;
 import ru.nsu.worksdonttouch.cardholder.kotlinclient.data.data.card.Card;
 import ru.nsu.worksdonttouch.cardholder.kotlinclient.data.data.card.CardList;
 import ru.nsu.worksdonttouch.cardholder.kotlinclient.net.ApiWorker;
@@ -12,10 +14,17 @@ import ru.nsu.worksdonttouch.cardholder.kotlinclient.net.objects.SimpleHttpResul
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 import java.util.Map;
 
 @SuppressWarnings("unused")
 public class ApiRequests extends ApiWorker {
+    protected UserData user;
+
+    public ApiRequests(UserData user) {
+        this.user = user;
+    }
 
     static class Gist {
         Map<String, GistFile> files;
@@ -39,19 +48,46 @@ public class ApiRequests extends ApiWorker {
         return true;
     }
 
+    static String authorizationString(UserData data) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            return "Basic " + new String(Base64.getEncoder()
+                    .encode((data.getLogin() + ":" + data.getPassword()).getBytes(StandardCharsets.UTF_8)));
+        }
+        return null;
+    }
+
     @Override
     public void changePassword(String password, HttpCallback<SimpleHttpResult> callback) throws Exception {
+        RequestBody formBody = new FormBody.Builder()
+                .build();
+        Request request = new Request.Builder()
+                .url("http://localhost:8080/v1.0/")
+                .addHeader("Password", password)
+                .addHeader("Authorization", authorizationString(user))
+                .post(formBody)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            if (!checkResponse(response)) {
+                callback.answer(HttpCallback.HttpResult.FAIL, null);
+            }
 
+            SimpleHttpResult simpleHttpResult = null;
+            callback.answer(HttpCallback.HttpResult.SUCCESSFUL, simpleHttpResult);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void getCardList(double latitude, double longitude, HttpCallback<CardList> callback) throws Exception {
-        RequestBody formBody = new FormBody.Builder()
-                .add("coords", latitude + ":" + longitude)
+        HttpUrl url = HttpUrl.parse("http://localhost:8080/v1.0/").newBuilder()
+                .addQueryParameter("latitude", latitude + "")
+                .addQueryParameter("longitude", longitude + "")
                 .build();
         Request request = new Request.Builder()
-                .url("http://localhost:8080/v1.0/")
-                .post(formBody)
+                .addHeader("Authorization", authorizationString(user))
+                .url(url)
                 .build();
 
         try (Response response = client.newCall(request).execute()) {
@@ -69,12 +105,12 @@ public class ApiRequests extends ApiWorker {
 
     @Override
     public void getCard(long id, HttpCallback<Card> callback) throws Exception {
-        RequestBody formBody = new FormBody.Builder()
-                .add("cardId", id + "")
+        HttpUrl url = HttpUrl.parse("http://localhost:8080/v1.0/").newBuilder()
+                .addQueryParameter("cardId", id + "")
                 .build();
         Request request = new Request.Builder()
-                .url("http://localhost:8080/v1.0/")
-                .post(formBody)
+                .addHeader("Authorization", authorizationString(user))
+                .url(url)
                 .build();
 
         try (Response response = client.newCall(request).execute()) {
@@ -93,9 +129,10 @@ public class ApiRequests extends ApiWorker {
     @Override
     public void editCard(Card card, HttpCallback<Card> callback) throws Exception {
         RequestBody formBody = new FormBody.Builder()
-                .add("card", card + "")
+                .add("card", card.toString())
                 .build();
         Request request = new Request.Builder()
+                .addHeader("Authorization", authorizationString(user))
                 .url("http://localhost:8080/v1.0/")
                 .post(formBody)
                 .build();
@@ -115,9 +152,10 @@ public class ApiRequests extends ApiWorker {
     @Override
     public void addCard(Card card, HttpCallback<Card> callback) throws Exception {
         RequestBody formBody = new FormBody.Builder()
-                .add("card", card + "")
+                .add("card", card.toString())
                 .build();
         Request request = new Request.Builder()
+                .addHeader("Authorization", authorizationString(user))
                 .url("http://localhost:8080/v1.0/")
                 .post(formBody)
                 .build();
@@ -136,11 +174,73 @@ public class ApiRequests extends ApiWorker {
 
     @Override
     public void imageGet(long id, File toWrite, HttpCallback<File> callback) throws Exception {
+        HttpUrl url = HttpUrl.parse("http://localhost:8080/v1.0/").newBuilder()
+                .addQueryParameter("imageId", id + "")
+                .build();
+        Request request = new Request.Builder()
+                .addHeader("Authorization", authorizationString(user))
+                .url(url)
+                .build();
 
+        try (Response response = client.newCall(request).execute()) {
+            if (!checkResponse(response)) {
+                callback.answer(HttpCallback.HttpResult.FAIL, null);
+            }
+
+            File file = null;
+            callback.answer(HttpCallback.HttpResult.SUCCESSFUL, file);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
-    public void uploadImage(long id, HttpCallback<ImageAnswer> callback) throws Exception {
+    public void uploadImage(File file, long id, HttpCallback<ImageAnswer> callback) throws Exception {
+        MultipartBody body = new MultipartBody.Builder()
+                .addFormDataPart("cardId", id + "")
+                .addFormDataPart("file", file.getName(), RequestBody.create(file, MediaType.parse("image/png")))
+                .build();
+        Request request = new Request.Builder()
+                .addHeader("Authorization", authorizationString(user))
+                .url("http://localhost:8080/v1.0/")
+                .post(body)
+                .build();
 
+        try (Response response = client.newCall(request).execute()) {
+            if (!checkResponse(response)) {
+                callback.answer(HttpCallback.HttpResult.FAIL, null);
+            }
+
+            callback.answer(HttpCallback.HttpResult.SUCCESSFUL, null);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void editImage(File file, long id, HttpCallback<ImageAnswer> callback) throws Exception {
+        MultipartBody body = new MultipartBody.Builder()
+                .addFormDataPart("cardId", id + "")
+                .addFormDataPart("file", file.getName(), RequestBody.create(file, MediaType.parse("image/png")))
+                .build();
+        Request request = new Request.Builder()
+                .addHeader("Authorization", authorizationString(user))
+                .url("http://localhost:8080/v1.0/")
+                .post(body)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (!checkResponse(response)) {
+                callback.answer(HttpCallback.HttpResult.FAIL, null);
+            }
+
+
+            callback.answer(HttpCallback.HttpResult.SUCCESSFUL, null);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
