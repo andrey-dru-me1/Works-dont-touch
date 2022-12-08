aae431a77a47038b35a15fb96780b92312ebfdc1
MihailCCfit <m.tsukanov@g.nsu.ru>
2022-12-09 02:28:51 +0700

    add getCardList

--- KotlinClient/app/src/main/java/ru/nsu/worksdonttouch/cardholder/kotlinclient/net/requests/ApiRequests.java
+++ KotlinClient/app/src/main/java/ru/nsu/worksdonttouch/cardholder/kotlinclient/net/requests/ApiRequests.java
@@ -63,6 +63,28 @@ public class ApiRequests extends ApiWorker {
             callback.answer(HttpCallback.HttpResult.NO_CONNECTION, null);
         }
     }
+    @Override
+    public void getCardList(HttpCallback<CardList> callback) {
+        HttpUrl url = Configuration.basicBuilder().addPathSegments("cards/getList")
+                .build();
+        Request request = new Request.Builder()
+                .addHeader("Authorization", authorizationString(user))
+                .url(url)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful() || response.body() == null) {
+                callback.answer(HttpCallback.HttpResult.errorHandler(response), null);
+                return;
+            }
+            CardList cardList = objectMapper.readValue(response.body().string(), CardList.class);
+            callback.answer(HttpCallback.HttpResult.SUCCESSFUL, cardList);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            callback.answer(HttpCallback.HttpResult.NO_CONNECTION, null);
+        }
+    }
 
     @Override
     public void getCardList(double latitude, double longitude, HttpCallback<CardList> callback) {
@@ -271,6 +293,7 @@ public class ApiRequests extends ApiWorker {
             callback.answer(HttpCallback.HttpResult.NO_CONNECTION, null);
         }
     }
+
 
     @Override
     public void deleteCard(Card card, HttpCallback<Card> callback) {
